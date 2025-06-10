01c3632acae8ae5ef3fef87d2e5e8ecbfe1df161
Ahad Khan <kahad316@gmail.com>
2025-06-10 14:30:01 +0500

    again file has been updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -69,7 +69,7 @@ export default defineConfig(({ mode }) => {
     },
 
     build: {
-      outDir: "/dist/",
+      outDir: "dist",
       assetsDir: "assets",
       rollupOptions: {
         output: {
